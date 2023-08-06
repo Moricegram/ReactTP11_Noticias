51f216af71bb2dafd42985803f2dc1adc3901dff
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-08-06 12:18:48 -0300

    Agregando logica y States a Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,31 +1,87 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import ListaNoticias from './ListaNoticias';
-
+import { useEffect, useState } from 'react'
+import { Button, Container } from 'react-bootstrap';
 
 const Formulario = () => {
+
+//Necesitaré un state para almacenar la respuesta de una API, por lo que creo a "noticias"
+  //Lo inicializo con un objeto vacio, porque la respuesta es un objeto.
+  const [categoria,setCategoria] = useState("");
+  const [noticia,setNoticia] = useState([]);
+  const [verNoticia,setVerNoticia] = useState(false);
+  
+    
+  //Consulto a la API mediante el hook useEffect(), con su propiedad [] para carga solo en montaje.
+  useEffect( ()=> {
+    consultarApi();
+  },[])
+
+//   La respuesta en este caso devuelve un Objeto en Formato JSON. Luego hago una solicitud a una API mediante una funcion, consultarApi().
+//Cada vez que solicite informacion fuera de mi proyecto, y que la informacion pueda fallar, lo hago mediante el uso de la estructura try catch.
+//Utilizo async y awit para resolver la promesa de la respuesta que no es inmediata a la solicitud de la API.
+const consultarApi = async () => {
+  try {
+    //Codigo que ejecutaré.
+    //Para solicitar informacion a una API, la hare mediante un Fetch. (De JS nativo). Lo almaceno en una variable "respuesta"
+    const respuesta = await fetch(
+        "https://newsdata.io/api/1/news?apikey=pub_26952eea98f9f0a904f0f5479e75595644894&q&language=es&category=world"
+      );
+      //Ahora para extraer el Response del Body, creo una constante "dato" para recuperar esa informacion.
+      const dato = await respuesta.json();
+      console.log(respuesta);
+      console.log(dato);
+      // Ahora tomare esa respuesta y la guardaré en el State noticia, mediante el uso de setNoticia.
+      setNoticia(dato);
+      //Ahora quiero quiero que ese Estado se vea en el componente hijo, y se lo envio por Props.
+      //Al obtener los tados de la API, cambio verNoticias a True.
+      setVerNoticia(true);
+      console.log(verNoticia);
+      console.log(noticia)
+  } catch (error) {
+    console.log(error);
+  };
+};
+
+const handleSubmit = (e) => {
+    e.preventDefault ();
+    setCategoria(e.target.value);
+    console.log(categoria);
+    consultarApi();
+};
+
     return (
         <>
-            <Form className="mt-2 bg-dark text-white">
+            <Form className="mt-2 bg-dark text-white" onSubmit={handleSubmit}>
                 <Form.Group className="my-2 d-flex px-2" controlId="noticias">
                 <Form.Label className="text-light col-5">Buscar por Categoria</Form.Label>
-                <Form.Select>
-                    <option value="1">Negocio</option>
-                    <option value="2">Entretenimiento</option>
-                    <option value="3">Ambiente</option>
-                    <option value="4">Alimento</option>
-                    <option value="5">Salud</option>
-                    <option value="6">Política</option>
-                    <option value="7">Ciencia</option>
-                    <option value="8">Deportes</option>
-                    <option value="9">Tecnología</option>
-                    <option value="10">Top</option>
-                    <option value="11">Turismo</option>
-                    <option value="12">Mundo</option>
+                <Form.Select onChange={
+                  (e) => setCategoria(e.target.value)}
+                  value={categoria}
+                  > 
+                    <option value="business">Negocio</option>
+                    <option value="entertainment">Entretenimiento</option>
+                    <option value="environment">Ambiente</option>
+                    <option value="food">Alimento</option>
+                    <option value="health">Salud</option>
+                    <option value="politics">Política</option>
+                    <option value="science">Ciencia</option>
+                    <option value="sports">Deportes</option>
+                    <option value="technology">Tecnología</option>
+                    <option value="top">Top</option>
+                    <option value="tourism">Turismo</option>
+                    <option value="world">Mundo</option>
                 </Form.Select>
+                  <Button
+                    variant="secondary"
+                    type="submit"
+                  >
+                    Buscar
+                  </Button>
                 </Form.Group>
             </Form>
-            <ListaNoticias></ListaNoticias>
+            <ListaNoticias propsNoticia={noticia}></ListaNoticias>
         </>
     );
 };
--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Noticia from './Noticia';
 
-const ListaNoticias = () => {
+const ListaNoticias = ({propsNoticia}) => {
     return (
-        <div className="d-flex row m-0 p-o">
-            <Noticia></Noticia>
-            <Noticia></Noticia>
-            <Noticia></Noticia>
-            <Noticia></Noticia>
-            <Noticia></Noticia>
-            <Noticia></Noticia>
-            <Noticia></Noticia>
-            <Noticia></Noticia>
-            
-        </div>
+        // <div className="d-flex row m-0 p-o">
+        //     <Noticia ></Noticia>
+        //     <Noticia></Noticia>
+        //     <Noticia></Noticia>
+        //     <Noticia></Noticia>
+        //     <Noticia></Noticia>
+        //     <Noticia></Noticia>
+        //     <Noticia></Noticia>
+        //     <Noticia></Noticia>            
+        // </div>
+        <>
+        <Card style={{ width: '16rem' }} className='m-2'>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+            <p><i> Diario 1</i></p>
+            <Card.Title>Titulo Noticia</Card.Title>
+            <Card.Text>
+            Resumen de la Noticia
+            </Card.Text>
+            <div className='text-center'>
+            <Button variant="secondary">Ver Noticia Completa</Button>
+            </div>
+        </Card.Body>
+        </Card>
+        </>
     );
 };
 
